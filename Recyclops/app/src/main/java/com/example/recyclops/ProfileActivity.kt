b74794f5b8b6f8b853fe86c18f56b6bdbef3c515
Thomas Braccia <bracciata@gmail.com>
2020-01-18 13:50:07 -0600

    Created objects folder and added methods for a friends list.

--- Recyclops/app/src/main/java/com/example/recyclops/ProfileActivity.kt
+++ Recyclops/app/src/main/java/com/example/recyclops/ProfileActivity.kt
@@ -17,10 +17,24 @@ class ProfileActivity : AppCompatActivity() {
     private fun populateProfile() {
         setContentView(R.layout.activity_profile)
         // Populate profile
+        val incomingRequestIds = getIncomingFriendRequests()
     }
     private fun returnToMain(){
         val intent = Intent(this, MainActivity::class.java)
         // start your next activity
         startActivity(intent)
+    }
+    private fun addFriend(){
+
+    }
+    private fun acceptFriendRequest(){
+
+    }
+    private fun rejectFriendRequests(){
+
+    }
+    private fun getIncomingFriendRequests(){
+        val incomingRequestIds = arrayOf("hey","Here")
+        return incomingRequestIds
     }
 }
