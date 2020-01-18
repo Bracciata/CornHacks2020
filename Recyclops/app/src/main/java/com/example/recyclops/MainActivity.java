733e8d6b3c9c27ef61b0e0de88014321450895f1
Thomas Braccia <bracciata@gmail.com>
2020-01-18 11:11:02 -0600

    Adding code from guide to set up tensorflow

--- Recyclops/app/src/main/java/com/example/recyclops/MainActivity.java
+++ Recyclops/app/src/main/java/com/example/recyclops/MainActivity.java
@@ -4,11 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+private val REQUEST_CODE_PERMISSIONS = 101
+private val REQUIRED_PERMISSIONS = arrayOf("android.permission.CAMERA")
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (allPermissionsGranted()) {
+        textureView.post { startCamera() }
+        textureView.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+        updateTransform()
+        }
+        } else {
+        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+        }
+
+        override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+        ) {
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+        if (allPermissionsGranted()) {
+        startCamera()
+        } else {
+        Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT)
+        .show()
+        finish()
+        }
+        }
+        }
+
+private fun allPermissionsGranted(): Boolean {
+
+        for (permission in REQUIRED_PERMISSIONS) {
+        if (ContextCompat.checkSelfPermission(
+        this,
+        permission
+        ) != PackageManager.PERMISSION_GRANTED
+        ) {
+        return false
+        }
+        }
+        return true
+        }
+        }
