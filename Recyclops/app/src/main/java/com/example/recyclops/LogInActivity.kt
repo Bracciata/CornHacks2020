311bf8c4fc9066fc9bde7a5374b1b8e604901737
dannyT2018 <danny.tran2018@gmail.com>
2020-01-19 02:42:47 -0600

    Formatted LoginActivity according to coding convention.

--- Recyclops/app/src/main/java/com/example/recyclops/LogInActivity.kt
+++ Recyclops/app/src/main/java/com/example/recyclops/LogInActivity.kt
@@ -1,5 +1,6 @@
 package com.example.recyclops
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -20,27 +21,29 @@ class LogInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setupLogIn()
     }
+
     private fun setupLogIn(){
         setContentView(R.layout.activity_log_in)
         // Get the list of all users
         // Note the following is temporary as later on it will be based off of an online database.
         val listOfUsers: List<User> = getUsers()
         // get reference to button
-        val signInButton = findViewById(R.id.signInButton) as Button
+        val signInButton = findViewById<Button>(R.id.signInButton)
         // set on-click listener
         signInButton.setOnClickListener {
             attemptSignIn(listOfUsers)
         }
-        val registerButton = findViewById(R.id.registerButton) as Button
+        val registerButton = findViewById<Button>(R.id.registerButton)
         // set on-click listener
         registerButton.setOnClickListener {
             openRegister()
         }
-        var toolbar : Toolbar = findViewById(R.id.toolbar)
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
     }
+
     // actions on click menu items
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         android.R.id.home -> {
@@ -53,28 +56,32 @@ class LogInActivity : AppCompatActivity() {
             super.onOptionsItemSelected(item)
         }
     }
+
     private fun returnToMain(){
         val intent = Intent(this, MainActivity::class.java)
         // start your next activity
         startActivity(intent)
     }
+
     private fun getUsers():List<User>{
         val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,
             Context.MODE_PRIVATE)
         val userJson = sharedPreferences.getString("users_key","{}")
-        val userList:  List<User> = Gson().fromJson(userJson, Array<User>::class.java).toList()
-        return userList
+        return Gson().fromJson(userJson, Array<User>::class.java).toList()
     }
+
     private fun setSignedInUser(userFound: User){
         val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile,Context.MODE_PRIVATE)
         val editor: SharedPreferences.Editor =  sharedPreferences.edit()
         val usersJson = Gson().toJson(userFound)
         editor.putString("active_user_key",usersJson)
-        editor.commit()
+        editor.apply()
     }
+
+    @SuppressLint("DefaultLocale")
     private fun attemptSignIn(listOfUsers:List<User>){
-        val emailEditText = findViewById(R.id.userEmailEditText) as EditText
-        val passwordEditText = findViewById(R.id.userPasswordEditText) as EditText
+        val emailEditText = findViewById<EditText>(R.id.userEmailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.userPasswordEditText)
         val password = passwordEditText.text.toString()
         val email = emailEditText.text.toString()
         for(user in listOfUsers){
@@ -83,7 +90,7 @@ class LogInActivity : AppCompatActivity() {
                 // because there will not be two users with same email
                 if(user.checkPassword(password)){
                     // Create toast that you are signed
-                    var firstName = user.firstName
+                    val firstName = user.firstName
                     Toast.makeText(this, "Hello, $firstName.", Toast.LENGTH_SHORT).show()
                     // Open profile
                     setSignedInUser(user)
@@ -95,7 +102,6 @@ class LogInActivity : AppCompatActivity() {
         // Failed to find correct user
         // Create toast
         Toast.makeText(this, "Sorry we could not find an account with those credentials. Try again!", Toast.LENGTH_SHORT).show()
-
     }
 
     private fun openProfile(){
@@ -104,6 +110,7 @@ class LogInActivity : AppCompatActivity() {
         // start your next activity
         startActivity(intent)
     }
+
     private fun openRegister(){
         // Opens the registration screen.
         val intent = Intent(this, RegisterActivity::class.java)
