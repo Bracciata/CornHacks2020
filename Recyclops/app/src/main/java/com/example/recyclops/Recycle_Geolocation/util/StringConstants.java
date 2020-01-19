944fa46ba610393e9824075a52ffc0e08d91a930
dannyT2018 <danny.tran2018@gmail.com>
2020-01-18 18:31:59 -0600

    Adding dependency for map feature.

---
+++ Recyclops/app/src/main/java/com/example/recyclops/Recycle_Geolocation/util/StringConstants.java
@@ -0,0 +1,10 @@
+package com.example.recyclops.Recycle_Geolocation.util;
+
+/**
+ * Class for constant Strings used in the project
+ */
+public class StringConstants {
+
+  public static final String SELECTED_THEME = "SELECTED THEME";
+
+}
